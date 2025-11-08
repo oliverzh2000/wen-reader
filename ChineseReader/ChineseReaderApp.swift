b0e35eb5fd68d62b4b9585a872525452e6dd0e92
Oliver Zhang <oliverzh2000@gmail.com>
2025-11-08 12:20:33 -0500

    Initial Commit

---
+++ ChineseReader/ChineseReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChineseReaderApp.swift
+//  ChineseReader
+//
+//  Created by Oliver Zhang on 2025-11-08.
+//
+
+import SwiftUI
+
+@main
+struct ChineseReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
